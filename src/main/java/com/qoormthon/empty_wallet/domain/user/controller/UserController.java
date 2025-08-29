2bba41ceb37d7eb7cc3d51cb01f9edb6671b04f7
june <0721hjp@naver.com>
2025-08-30 00:19:27 +0900

    chore: 스웨거 문서화 작업을 위한 패키지 추가

--- src/main/java/com/qoormthon/empty_wallet/domain/user/controller/UserController.java
+++ src/main/java/com/qoormthon/empty_wallet/domain/user/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.qoormthon.empty_wallet.domain.user.controller;
 
+import com.qoormthon.empty_wallet.domain.user.docs.UserDocs;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class UserController implements UserDocs {
 
 }
---
+++ src/main/java/com/qoormthon/empty_wallet/domain/user/docs/UserDocs.java
@@ -0,0 +1,5 @@
+package com.qoormthon.empty_wallet.domain.user.docs;
+
+public interface UserDocs {
+
+}
