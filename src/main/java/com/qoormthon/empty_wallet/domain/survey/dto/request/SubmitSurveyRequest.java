a89a11848ca9021da8eee6ee3c3ebb1cde214f96
zzingni <wldms001313@gamil.com>
2025-09-02 03:30:38 +0900

    Feat: 사용자 제출 답안 요청

---
+++ src/main/java/com/qoormthon/empty_wallet/domain/survey/dto/request/SubmitSurveyRequest.java
@@ -0,0 +1,13 @@
+package com.qoormthon.empty_wallet.domain.survey.dto.request;
+
+import com.qoormthon.empty_wallet.domain.survey.entity.SurveyType;
+
+import java.util.List;
+
+// 사용자가 선택한 답안 제출 요청
+public record SubmitSurveyRequest(
+        SurveyType type,                 // FULL | QUICK
+        List<AnswerRequest> answers          // 질문ID + 보기 타입(A~F)
+) {
+    public record AnswerRequest(Long surveyId, String optionType) {}
+}
