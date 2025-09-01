0c6eef549d9b174f648765b36afd9a3d7c2d1405
june <0721hjp@naver.com>
2025-09-01 12:46:14 +0900

    Feat: 엑세스토큰 발급 api 추가

--- src/main/java/com/qoormthon/empty_wallet/domain/auth/controller/AuthController.java
+++ src/main/java/com/qoormthon/empty_wallet/domain/auth/controller/AuthController.java
@@ -1,7 +1,29 @@
 package com.qoormthon.empty_wallet.domain.auth.controller;
 
 import com.qoormthon.empty_wallet.domain.auth.docs.AuthDocs;
+import com.qoormthon.empty_wallet.domain.auth.service.AuthService;
+import com.qoormthon.empty_wallet.global.common.dto.response.ResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController implements AuthDocs {
+
+  private final AuthService authService;
+
+  @Override
+  @PostMapping("/access-token")
+  public ResponseDTO<String> issueAccessToken(HttpServletRequest request) {
+    String accessToken = authService.issueAccessToken(request);
+
+    return ResponseDTO.of(accessToken, "엑세스 토큰 발급에 성공하였습니다.");
+  }
 
 }
--- src/main/java/com/qoormthon/empty_wallet/domain/auth/docs/AuthDocs.java
+++ src/main/java/com/qoormthon/empty_wallet/domain/auth/docs/AuthDocs.java
@@ -1,7 +1,11 @@
 package com.qoormthon.empty_wallet.domain.auth.docs;
 
+import com.qoormthon.empty_wallet.global.common.dto.response.ResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
 public interface AuthDocs {
 
-
+  ResponseDTO<String> issueAccessToken(HttpServletRequest request);
 
 }
